44ab0c19375cf8a9c21139306fd0c3e4178226d7
wargo <wojciech.r@op.pl>
2015-07-07 01:22:39 +0200

    Version

--- src/clientversion.cpp
+++ src/clientversion.cpp
@@ -13,7 +13,7 @@
  * for both bitcoind and bitcoin-core, to make it harder for attackers to
  * target servers or GUI users specifically.
  */
-const std::string CLIENT_NAME("PolishCoin - Satoshi");
+const std::string CLIENT_NAME("PolishCoin");
 
 /**
  * Client version number
